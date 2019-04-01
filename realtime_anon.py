5cc3cbd5a5cc06315c6397fac02e4f7b5c01a83b
Abhi Sharma <18308855+SocialSharma@users.noreply.github.com>
2019-03-31 18:04:14 -0700

    [add] clarify realtime anon functions

--- realtime_anon.py
+++ realtime_anon.py
@@ -5,43 +5,39 @@ from sklearn.cluster import KMeans
 import numpy as np
 
 
-def hog_detector(image, gray):
-    # Get faces into webcam's image
+def anonymizer(image, gray):
+    """Anonymize faces using keypoints and clustering on biometric zones."""
     rects = detector(gray, 0)
-    # For each detected face, find the landmark.
     for (i, rect) in enumerate(rects):
-        # Make the prediction and transfom it to numpy array
+        # Predict keypoints
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
 
         # Cluster
-        ZZ = np.array(shape)
-        kmeans = KMeans(n_clusters=3, random_state=0).fit(ZZ)
+        group = np.array(shape)
+        kmeans = KMeans(n_clusters=3, random_state=0).fit(group)
         ppa = [x[0] for x in kmeans.cluster_centers_]
         ppb = [x[1] for x in kmeans.cluster_centers_]
 
+        # Draw cluster on biometric identity zones
         for a, b in zip(ppa, ppb):
             a = int(a)
             b = int(b)
             cv2.circle(image, (a, b), 50, (0, 0, 0), -1)
                    
 
-def facial_landmarks():
+def process_video():
+    """Process video and handle user commands."""
     # Feed from computer camera with threading
     cap = video.VideoStream(src=0).start()
 
     while True:
-        # Getting out image by webcam
+        # Read image, anonymize, and wait for user quit.
         image = cap.read()
-
-        # Converting the image to gray scale
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        hog_detector(image, gray)
-
-        # show the output frame
-        # adding brightness and contrast -> α⋅p(i,j)+β where p(i.j) is pixel value for each point
+        anonymizer(image, gray)
         image = cv2.convertScaleAbs(image, alpha=1.0, beta=0)
-        cv2.imshow("Facial Landmarks", image)
+        cv2.imshow("Realtime Anon", image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -51,4 +47,4 @@ def facial_landmarks():
 if __name__ == "__main__":
     predictor = dlib.shape_predictor('model.dat')
     detector = dlib.get_frontal_face_detector()
-    facial_landmarks()
+    process_video()
